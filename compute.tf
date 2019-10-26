46321159120cc6d4c4c18a2994b2f7dad802a944
kriskrku <kriskrku@KRISKRKU-IN.in.oracle.com>
2019-10-26 17:58:23 +0530

    add compute

--- compute.tf
+++ compute.tf
@@ -46,10 +46,9 @@ data "oci_core_vnic_attachments" "instance_vnics" {
 }
 
 
-output "primary_ip_addresses" {
-  value = ["${oci_core_instance.test_instance.public_ip}",
-    "${oci_core_instance.test_instance.private_ip}",
-  ]
+# Gets the OCID of the first (default) VNIC
+data "oci_core_vnic" "instance_vnic" {
+  vnic_id = "${lookup(data.oci_core_vnic_attachments.instance_vnics.vnic_attachments[0],"vnic_id")}"
 }
 
 
