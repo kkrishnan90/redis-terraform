8b874bf2983d22c0b89e76ae48010cc3b088fd09
kriskrku <kriskrku@KRISKRKU-IN.in.oracle.com>
2019-10-26 18:32:17 +0530

    add compute

--- compute.tf
+++ compute.tf
@@ -5,7 +5,7 @@ resource "oci_core_instance" "TestInstance" {
   compartment_id      = "${var.compartment_ocid}"
   display_name        = "TestInstance"
   shape               = "${var.instance_shape}"
-  
+  image = "${var.instance_image_ocid[var.region]}"
   create_vnic_details{
     subnet_id = "${var.subnet_ocid}"
     display_name     = "primaryvnic"
@@ -13,16 +13,6 @@ resource "oci_core_instance" "TestInstance" {
     hostname_label   = "TestInstance"
   }
 
-
-  source_details {
-    source_type = "image"
-    source_id   = "${var.instance_image_ocid[var.region]}"
-
-    # Apply this to set the size of the boot volume that's created for this instance.
-    # Otherwise, the default boot volume size of the image is used.
-    # This should only be specified when source_type is set to "image".
-    #boot_volume_size_in_gbs = "60"
-  }
 
   # Apply the following flag only if you wish to preserve the attached boot volume upon destroying this instance
   # Setting this and destroying the instance will result in a boot volume that should be managed outside of this config.
