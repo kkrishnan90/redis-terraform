d86fd9cc087df1888f8ca019722e4d1eab599351
kriskrku <kriskrku@KRISKRKU-IN.in.oracle.com>
2019-10-26 18:53:41 +0530

    add compute

--- variables.tf
+++ variables.tf
@@ -40,6 +40,7 @@ variable "instance_image_ocid" {
   default = {
     # See https://docs.us-phoenix-1.oraclecloud.com/images/
     # Oracle-provided image "Oracle-Linux-7.5-2018.10.16-0"
+    ap-mumbai-1 = "ocid1.image.oc1.ap-mumbai-1.aaaaaaaapkhghx7pslvwrarurrztge4f4a3vlfy3kt3jd7vtxz4przfkmfvq"
     us-phoenix-1 = "ocid1.image.oc1.phx.aaaaaaaaoqj42sokaoh42l76wsyhn3k2beuntrh5maj3gmgmzeyr55zzrwwa"
 
     us-ashburn-1   = "ocid1.image.oc1.iad.aaaaaaaageeenzyuxgia726xur4ztaoxbxyjlxogdhreu3ngfj2gji3bayda"
