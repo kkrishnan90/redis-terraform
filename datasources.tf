c810464be984d522cc2355332c44501837ddbe9a
kriskrku <kriskrku@KRISKRKU-IN.in.oracle.com>
2019-10-26 18:25:50 +0530

    add compute

--- datasources.tf
+++ datasources.tf
@@ -1,5 +1,6 @@
 // Copyright (c) 2017, 2019, Oracle and/or its affiliates. All rights reserved.
 
+
 data "oci_identity_availability_domain" "ad" {
   compartment_id = "${var.tenancy_ocid}"
   ad_number      = 1
