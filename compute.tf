48e9f536048b703999347b26c3be3d0831c6a7b1
kriskrku <kriskrku@KRISKRKU-IN.in.oracle.com>
2019-10-28 22:52:44 +0530

    complete HAProxy add 10 additional IPs to Primary vNIC

--- compute.tf
+++ compute.tf
@@ -77,6 +77,6 @@ output "templateOuput" {
 
 
 data "template_file" "user_data" { 
-  template = "${base64encode(templatefile("./userdata/bootstrap.tpl",{primaryip="${oci_core_instance.TestInstance.private_ip}"}))}"
+  template = "${base64encode(templatefile("./userdata/bootstrap.tpl",{primaryip=${combined_data}}))}"
 }
 
