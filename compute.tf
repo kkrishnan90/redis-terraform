1a6569d72bd802599e49cc53427bda96e0bcff04
kriskrku <kriskrku@KRISKRKU-IN.in.oracle.com>
2019-10-28 09:09:57 +0530

    complete HAProxy add 10 additional IPs to Primary vNIC

--- compute.tf
+++ compute.tf
@@ -51,7 +51,7 @@ data "oci_core_private_ips" "private_ip_datasource" {
 }
 
 output "private_ips" {
-  value = ["${data.oci_core_private_ips.private_ip_datasource.private_ips}"]
+  value = ["${lookup(data.oci_core_private_ips.private_ip_datasource.private_ips[0],"ip_address")}"]
 }
 
 output "InstancePrivateIPs" {
