7c7076d2475b53f567d7d8c7b1824ff296a62d12
kriskrku <kriskrku@KRISKRKU-IN.in.oracle.com>
2019-10-28 19:38:59 +0530

    complete HAProxy add 10 additional IPs to Primary vNIC

--- compute.tf
+++ compute.tf
@@ -61,7 +61,7 @@ output "primary-private-ip" {
 
 data "template_file" "user_data" {
 template = "${base64encode(file("bootstrap.sh"))}"
-vars {
+vars = {
   ip = "${oci_core_instance.TestInstance.private_ip}"
 }
 }
