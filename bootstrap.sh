7001c0e9982f4b362dd26ca6b603b0b636527faf
kriskrku <kriskrku@KRISKRKU-IN.in.oracle.com>
2019-10-28 19:10:34 +0530

    complete HAProxy add 10 additional IPs to Primary vNIC

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
  
 cp /etc/motd /home/opc/motd.bkp
+
+var1="VALUE 1"
+var2="VALUE 2"
+
+cat > /home/opc/file1.md << EOF1
+do some commands on "$var1" 
+and/or "$var2"
+EOF1
+
+cat > /home/opc/file2.md << "EOF2"
+do some commands on "$var1" 
+and/or "$var2"
+EOF2
