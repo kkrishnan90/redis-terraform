600907677b8bd4d05076e7e4d1452d7c197cbbf5
kriskrku <kriskrku@KRISKRKU-IN.in.oracle.com>
2019-10-29 13:04:36 +0530

    complete HAProxy add 10 additional IPs to Primary vNIC

--- compute.tf
+++ compute.tf
@@ -57,14 +57,14 @@ resource "oci_core_private_ip" "private_ip" {
   hostname_label = "somehostnamelabel${count.index}"
 
   provisioner "local-exec" {
-      command = << EOT 
+      command =" << EOT 
       touch privateips/ens3:${count.index}
       echo DEVICE=\"ens3:${count.index}\" >> privateips/ens3:${count.index}
       echo BOOTPROTO=static >> privateips/ens3:${count.index}
       echo IPADDR=${self.ip_address} >> privateips/ens3:${count.index}
       echo NETMASK=255.255.255.0 >> privateips/ens3:${count.index}
       echo ONBOOT=yes >> privateips/ens3:${count.index}
-      EOT
+      EOT"
     
   }
 
