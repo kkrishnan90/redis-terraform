0f04f34a044b743baab92373fbe356ce05b89fba
kriskrku <kriskrku@KRISKRKU-IN.in.oracle.com>
2019-10-29 16:47:07 +0530

    complete HAProxy add 10 additional IPs to Primary vNIC

--- compute.tf
+++ compute.tf
@@ -21,20 +21,6 @@ resource "oci_core_instance" "TestInstance" {
   timeouts {
     create = "60m"
   }
-
-  # provisioner "remote-exec" {    
-  #   inline = [
-  #     "cp /etc/motd /home/opc/motd.bkp",
-  #     "echo ip=${oci_core_instance.TestInstance.private_ip} > motd.bkp",
-  #   ]
-  # }
-  # connection {
-  #   type     = "ssh"
-  #   host = "${oci_core_instance.TestInstance.private_ip}"
-  #   user     = "opc"
-  #   password = ""
-  #   private_key = "${file("/home/opc/private_key_oci.pem")}"
-  # }
 }
 
 data "oci_core_vnic_attachments" "instance_vnics" {
@@ -59,29 +45,10 @@ resource "oci_core_private_ip" "private_ip" {
   provisioner "local-exec" {
       command = "touch privateips/ifcfg-ens3:${count.index}\necho DEVICE='\"ens3:${count.index}\"' >> privateips/ifcfg-ens3:${count.index}\necho BOOTPROTO=static >> privateips/ifcfg-ens3:${count.index}\necho IPADDR=${self.ip_address} >> privateips/ifcfg-ens3:${count.index}\necho NETMASK=255.255.255.0 >> privateips/ifcfg-ens3:${count.index}\necho ONBOOT=yes >> privateips/ifcfg-ens3:${count.index}"  
   }
-
-
-# provisioner "remote-exec" {    
-#     inline = [
-#       "echo ${self.ip_address} ${count.index} >> motd.bkp",
-#       # "sudo ip addr add ${self.ip_address}/24 dev ens3 label ens3:${count.index+1}",
-#       "echo ${self.ip_address} >> ips.txt"
-#       # "for ip in ${oci_core_private_ip.private_ip.*.ip_address} do echo ip > motd.bkp done"
-#     ]
-#   }
-#   connection {
-#     type     = "ssh"
-#     host = "${oci_core_instance.TestInstance.private_ip}"
-#     user     = "opc"
-#     password = ""
-#     private_key = "${file("/home/opc/private_key_oci.pem")}"
-#   }
-
 }
 
 # List Private IPs
 data "oci_core_private_ips" "private_ip_datasource" {
-  # depends_on = ["oci_core_private_ip.private_ip${count.index}"]
   vnic_id    = "${lookup(data.oci_core_vnic_attachments.instance_vnics.vnic_attachments[0],"vnic_id")}"
 
 }
@@ -91,12 +58,6 @@ resource "null_resource" "ansible" {
     command = "sudo ansible-playbook -i ${oci_core_instance.TestInstance.public_ip}, ansible/redis-playbook.yml --extra-vars variable_host=${oci_core_instance.TestInstance.public_ip}"
   }
 }
-
-# output "testipifcfg" {
-#   value = "touch privateips/ifcfg-ens3:1\necho DEVICE=\"ens3:1\" >> privateips/ens3:1\necho BOOTPROTO=static >> privateips/ens3:1\necho IPADDR=102.34.34.34 >> privateips/ens3:1\necho NETMASK=255.255.255.0 >> privateips/ens3:1\necho ONBOOT=yes >> privateips/ens3:1"
-# }
-
-
 
 # # output "private_ips" {
 # #   value = "${null_resource.ip_script}"
@@ -121,15 +82,4 @@ output "secondary-private-ips" {
 #   value = { primary_ip = "${oci_core_instance.TestInstance.private_ip}",secondary_ips="${oci_core_private_ip.private_ip.*.ip_address}" }
 # }
 
-
-
-# output "templateOuput" {
-#   value = "${data.template_file.user_data.rendered}"
-# }
-
-
-
-# data "template_file" "user_data" { 
-#   template = "${base64encode(templatefile("./userdata/bootstrap.tpl",{primaryip=""}))}"
-# }
 
