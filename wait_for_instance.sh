c8fe94da042770188c6314b76505dc75a9a76c38
kriskrku <kriskrku@KRISKRKU-IN.in.oracle.com>
2019-10-29 19:22:46 +0530

    complete HAProxy add 10 additional IPs to Primary vNIC

--- wait_for_instance.sh
+++ wait_for_instance.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-while [ ! -f /var/lib/cloud/instance/boot-finished ]: do
-    echo -e "\033[1;36Waiting for cloud-init..."
-    sleep 1
+
+while [ ! -f /var/lib/cloud/instance/boot-finished ]; do
+  echo -e "\033[1;36mWaiting for cloud-init..."
+  sleep 1
 done
