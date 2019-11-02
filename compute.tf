3f58dbff53b07477221467e4931bf12954655365
kriskrku <kriskrku@KRISKRKU-IN.in.oracle.com>
2019-11-02 20:38:06 +0530

    49

--- compute.tf
+++ compute.tf
@@ -38,7 +38,7 @@ data "oci_core_vnic_attachments" "instance_vnics" {
 # }
 
 output "vnic_ids" {
-  value =  "${data.oci_core_vnic_attachments.instance_vnics}"
+  value =  "${data.oci_core_vnic_attachments.instance_vnics.*.vnic_attachments[*]}"
 }
 
 
