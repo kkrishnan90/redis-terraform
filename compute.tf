4b0c2782564744d76b8b0d7ced82da8bc9245021
kriskrku <kriskrku@KRISKRKU-IN.in.oracle.com>
2019-11-02 21:13:47 +0530

    50

--- compute.tf
+++ compute.tf
@@ -38,7 +38,8 @@ data "oci_core_vnic_attachments" "instance_vnics" {
 # }
 
 output "vnic_ids" {
-  value =  "${values(zipmap(data.oci_core_vnic_attachments.instance_vnics[*].vnic_attachments[0].id, data.oci_core_vnic_attachments.instance_vnics[*].vnic_attachments[0].vnic_id))}"
+  # value =  "${values(zipmap(data.oci_core_vnic_attachments.instance_vnics[*].vnic_attachments[0].id, data.oci_core_vnic_attachments.instance_vnics[*].vnic_attachments[0].vnic_id))}"
+  value = "${data.oci_core_vnic_attachments.instance_vnics[*].vnic_attachments[0]}"
 }
 
 
