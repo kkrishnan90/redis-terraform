f385f585613f6d99e004df771d48ac32f2b1758f
kriskrku <kriskrku@KRISKRKU-IN.in.oracle.com>
2019-11-02 22:45:52 +0530

    50

--- compute.tf
+++ compute.tf
@@ -30,11 +30,11 @@ data "oci_core_vnic_attachments" "instance_vnics" {
 }
 
 locals {
-  name="${data.oci_core_vnic_attachments.instance_vnics[*].vnic_attachments[0][*]}"
+  name="${data.oci_core_vnic_attachments.instance_vnics[*].vnic_attachments[0]}"
 }
 
 output "locals-output" {
-  value =  "${local.name.vnic_id}"
+  value =  "${local.name.[*].vnic_id}"
 }
 
 # resource "oci_core_private_ip" "private_ip" {
