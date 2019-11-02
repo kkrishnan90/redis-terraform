7b2b535db78047280e2a01f038c830daa78782b3
kriskrku <kriskrku@KRISKRKU-IN.in.oracle.com>
2019-11-02 22:43:30 +0530

    50

--- compute.tf
+++ compute.tf
@@ -34,7 +34,7 @@ locals {
 }
 
 output "locals-output" {
-  value =  "${formatlist(local.name)}"
+  value =  "${tostring(local.name[*].vnic_id)}"
 }
 
 # resource "oci_core_private_ip" "private_ip" {
