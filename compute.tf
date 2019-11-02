d0e715f00916fb4599559c049010e5e45b9afa78
kriskrku <kriskrku@KRISKRKU-IN.in.oracle.com>
2019-11-03 00:34:51 +0530

    54

--- compute.tf
+++ compute.tf
@@ -31,7 +31,7 @@ data "oci_core_vnic_attachments" "instance_vnics" {
 }
 
 locals {
-  name="${data.oci_core_vnic_attachments.instance_vnics.*.vnic_attachments[0]}"
+  name="${data.oci_core_vnic_attachments.instance_vnics[*].vnic_attachments[0]}"
   # name = "${oci_core_instance.TestInstance[*].id}"
   vnics = {
     one = "${element(local.name.*.vnic_id, 0)}"
