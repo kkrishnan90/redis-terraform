33668783fdb2cfeb57aab5e916bd52420f5e5d68
kriskrku <kriskrku@KRISKRKU-IN.in.oracle.com>
2019-11-02 17:35:13 +0530

    1

--- compute.tf
+++ compute.tf
@@ -29,45 +29,50 @@ data "oci_core_vnic_attachments" "instance_vnics" {
   instance_id         = "${oci_core_instance.TestInstance.id}"
 }
 
-
-# Gets the OCID of the first (default) VNIC
-data "oci_core_vnic" "instance_vnic" {
-  vnic_id = "${lookup(data.oci_core_vnic_attachments.instance_vnics.vnic_attachments[0],"vnic_id")}"
+output "vnic_data" {
+  value = "${data.oci_core_vnic_attachments.instance_vnics}"
 }
 
-# Create PrivateIP
-resource "oci_core_private_ip" "private_ip" {
-  count = "${var.hap_ip_count}"
-  vnic_id        = "${lookup(data.oci_core_vnic_attachments.instance_vnics.vnic_attachments[0],"vnic_id")}"
-  display_name   = "someDisplayName${count.index}"
-  hostname_label = "somehostnamelabel${count.index}"
 
-  provisioner "local-exec" {
-      command = "touch privateips/ifcfg-ens3:${count.index}\necho DEVICE='\"ens3:${count.index}\"' >> privateips/ifcfg-ens3:${count.index}\necho BOOTPROTO=static >> privateips/ifcfg-ens3:${count.index}\necho IPADDR=${self.ip_address} >> privateips/ifcfg-ens3:${count.index}\necho NETMASK=255.255.255.0 >> privateips/ifcfg-ens3:${count.index}\necho ONBOOT=yes >> privateips/ifcfg-ens3:${count.index}"  
-  }
-}
 
-# List Private IPs
-data "oci_core_private_ips" "private_ip_datasource" {
-  vnic_id    = "${lookup(data.oci_core_vnic_attachments.instance_vnics.vnic_attachments[0],"vnic_id")}"
+# # Gets the OCID of the first (default) VNIC
+# data "oci_core_vnic" "instance_vnic" {
+#   vnic_id = "${lookup(data.oci_core_vnic_attachments.instance_vnics.vnic_attachments[0],"vnic_id")}"
+# }
 
-}
+# # Create PrivateIP
+# resource "oci_core_private_ip" "private_ip" {
+#   count = "${var.hap_ip_count}"
+#   vnic_id        = "${lookup(data.oci_core_vnic_attachments.instance_vnics.vnic_attachments[0],"vnic_id")}"
+#   display_name   = "someDisplayName${count.index}"
+#   hostname_label = "somehostnamelabel${count.index}"
 
-resource "null_resource" "ansible" {
-  provisioner "remote-exec" {
-    script="wait_for_instance.sh"
-  }
-  connection {
-    type     = "ssh"
-    host = "${oci_core_instance.TestInstance.private_ip}"
-    user     = "opc"
-    password = ""
-    private_key = "${file("/home/opc/private_key_oci.pem")}"
-  }
-  provisioner "local-exec"{
-    command = "sudo ansible-playbook -i ${oci_core_instance.TestInstance.public_ip}, ansible/redis-playbook.yml --extra-vars variable_host=${oci_core_instance.TestInstance.public_ip}"
-  }
-}
+#   provisioner "local-exec" {
+#       command = "touch privateips/ifcfg-ens3:${count.index}\necho DEVICE='\"ens3:${count.index}\"' >> privateips/ifcfg-ens3:${count.index}\necho BOOTPROTO=static >> privateips/ifcfg-ens3:${count.index}\necho IPADDR=${self.ip_address} >> privateips/ifcfg-ens3:${count.index}\necho NETMASK=255.255.255.0 >> privateips/ifcfg-ens3:${count.index}\necho ONBOOT=yes >> privateips/ifcfg-ens3:${count.index}"  
+#   }
+# }
+
+# # List Private IPs
+# data "oci_core_private_ips" "private_ip_datasource" {
+#   vnic_id    = "${lookup(data.oci_core_vnic_attachments.instance_vnics.vnic_attachments[0],"vnic_id")}"
+
+# }
+
+# resource "null_resource" "ansible" {
+#   provisioner "remote-exec" {
+#     script="wait_for_instance.sh"
+#   }
+#   connection {
+#     type     = "ssh"
+#     host = "${oci_core_instance.TestInstance.private_ip}"
+#     user     = "opc"
+#     password = ""
+#     private_key = "${file("/home/opc/private_key_oci.pem")}"
+#   }
+#   provisioner "local-exec"{
+#     command = "sudo ansible-playbook -i ${oci_core_instance.TestInstance.public_ip}, ansible/redis-playbook.yml --extra-vars variable_host=${oci_core_instance.TestInstance.public_ip}"
+#   }
+# }
 
 # # output "private_ips" {
 # #   value = "${null_resource.ip_script}"
@@ -79,14 +84,14 @@ resource "null_resource" "ansible" {
 #   value = "${oci_core_instance.TestInstance.private_ip}"
 # }
 
-output "coreip" {
-  value = "${lookup(oci_core_private_ip.private_ip[3], "ip_address")}"
-}
+# output "coreip" {
+#   value = "${lookup(oci_core_private_ip.private_ip[3], "ip_address")}"
+# }
 
 
-output "secondary-private-ips" {
-  value = "${oci_core_private_ip.private_ip.*.ip_address}"
-}
+# output "secondary-private-ips" {
+#   value = "${oci_core_private_ip.private_ip.*.ip_address}"
+# }
 
 # output "combined_data" {
 #   value = { primary_ip = "${oci_core_instance.TestInstance.private_ip}",secondary_ips="${oci_core_private_ip.private_ip.*.ip_address}" }
