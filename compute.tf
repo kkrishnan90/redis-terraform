7efd775a72871e6467f833e6883c22bcc3a088f3
kriskrku <kriskrku@KRISKRKU-IN.in.oracle.com>
2019-11-02 18:41:29 +0530

    29

--- compute.tf
+++ compute.tf
@@ -34,7 +34,7 @@ data "oci_core_vnic_attachments" "instance_vnics" {
 # Gets the OCID of the first (default) VNIC
 data "oci_core_vnic" "instance_vnic" {
   count = "${var.NumInstances}"
-  vnic_id = "${data.oci_core_vnic_attachments.instance_vnics.*.vnic_attachments[count.index]}"
+  vnic_id = "${element(data.oci_core_vnic_attachments.instance_vnics.*.vnic_attachments, count.index)}"
   # vnic_id = "${lookup(data.oci_core_vnic_attachments.instance_vnics.vnic_attachments[count.index],"vnic_id")}"
   # vnic_id = "${data.oci_core_vnic_attachments.instance_vnics.*.vnic_attachments}"
 }
