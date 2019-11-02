6b169c27f995c649203887513dae50a181ab5728
kriskrku <kriskrku@KRISKRKU-IN.in.oracle.com>
2019-11-02 18:25:24 +0530

    20

--- compute.tf
+++ compute.tf
@@ -31,16 +31,16 @@ data "oci_core_vnic_attachments" "instance_vnics" {
   instance_id         = "${oci_core_instance.TestInstance.*.id[count.index]}"
 }
 
-output "vnic_data" {
-  value = "${data.oci_core_vnic_attachments.instance_vnics[count.index].vnic_attachments[0]}"
+# Gets the OCID of the first (default) VNIC
+data "oci_core_vnic" "instance_vnic" {
+  count = "${var.NumInstances}"
+  vnic_id = "${lookup(data.oci_core_vnic_attachments.instance_vnics.*.vnic_attachments[0],"vnic_id")}"
 }
 
+output "vnic_ids" {
+  value = "${data.oci_core_vnic.instance_vnic}"
+}
 
-
-# # Gets the OCID of the first (default) VNIC
-# data "oci_core_vnic" "instance_vnic" {
-#   vnic_id = "${lookup(data.oci_core_vnic_attachments.instance_vnics.vnic_attachments[0],"vnic_id")}"
-# }
 
 # # Create PrivateIP
 # resource "oci_core_private_ip" "private_ip" {
