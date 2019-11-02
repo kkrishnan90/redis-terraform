f6cb65e7750932830054bec1a387d7f0b2e2555a
kriskrku <kriskrku@KRISKRKU-IN.in.oracle.com>
2019-11-02 18:15:08 +0530

    16

--- compute.tf
+++ compute.tf
@@ -24,15 +24,15 @@ resource "oci_core_instance" "TestInstance" {
   }
 }
 
-# data "oci_core_vnic_attachments" "instance_vnics" {
-#   count = "${var.NumInstances}"
-#   compartment_id      = "${var.compartment_ocid}"
-#   availability_domain = "${data.oci_identity_availability_domain.ad.name}"
-#   instance_id         = "${oci_core_instance.TestInstance.*.id[count.index]}"
-# }
+data "oci_core_vnic_attachments" "instance_vnics" {
+  count = "${var.NumInstances}"
+  compartment_id      = "${var.compartment_ocid}"
+  availability_domain = "${data.oci_identity_availability_domain.ad.name}"
+  instance_id         = "${oci_core_instance.TestInstance.*.id[count.index]}"
+}
 
 output "vnic_data" {
-  value = "${oci_core_instance.TestInstance}"
+  value = "${lookup(data.oci_core_vnic_attachments.instance_vnics.vnic_attachments[0],"vnic_id")}"
 }
 
 
