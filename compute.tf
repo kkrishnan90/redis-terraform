44c5b870da9deae5a673e5dfb797355af97afba0
kriskrku <kriskrku@KRISKRKU-IN.in.oracle.com>
2019-11-02 18:07:34 +0530

    13

--- compute.tf
+++ compute.tf
@@ -26,7 +26,6 @@ resource "oci_core_instance" "TestInstance" {
 
 data "oci_core_vnic_attachments" "instance_vnics" {
   count = "${var.NumInstances}"
-  depends_on = ["oci_core_instance.TestInstance"]
   compartment_id      = "${var.compartment_ocid}"
   availability_domain = "${data.oci_identity_availability_domain.ad.name}"
   instance_id         = "${oci_core_instance.TestInstance.*.id[count.index]}"
