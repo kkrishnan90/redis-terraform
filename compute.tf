19aa5c519680ebf2d104456ad929966d9c624005
kriskrku <kriskrku@KRISKRKU-IN.in.oracle.com>
2019-11-03 09:17:08 +0530

    54

--- compute.tf
+++ compute.tf
@@ -29,34 +29,32 @@ data "oci_core_vnic_attachments" "instance_vnics" {
   instance_id         = "${oci_core_instance.TestInstance.*.id[count.index]}"
 }
 
+# Gets the OCID of the first (default) VNIC
+data "oci_core_vnic" "instance_vnic" {
+  count = "${var.NumInstances}"
+  vnic_id = "${lookup(element(data.oci_core_vnic_attachments.instance_vnics.*.vnic_attachments[count.index],0),"vnic_id")}"
+}
+
 locals {
-  name="${data.oci_core_vnic_attachments.instance_vnics[*].vnic_attachments[0]}"
-  # name = "${oci_core_instance.TestInstance[*].id}"
-  vnics = {
-    one = "${element(local.name[*].vnic_id, 0)}"
-    two = "${element(local.name[*].vnic_id, 1)}"
-  }
-  check = ["Krishna", "Sweatha", "Latha", "Kumar"]
-  test = [for s in local.check: upper(s)]
-  init_count = "${length(oci_core_instance.TestInstance)}"
+ vnics = "${data.oci_core_vnic.instance_vnic}"
 }
 
 output "locals-output" {
-  value =  "${local.test}"
+  value =  "${local.vnics}"
   
 }
 
-resource "oci_core_private_ip" "private_ip" {
-  count = "${var.hap_ip_count}"
-  depends_on=["oci_core_instance.TestInstance"]
-  vnic_id        = "${element(local.name[*].vnic_id,count.index)}"
-  display_name   = "someDisplayName${count.index}"
-  hostname_label = "somehostnamelabel${count.index}"
+# resource "oci_core_private_ip" "private_ip" {
+#   count = "${var.hap_ip_count}"
+#   depends_on=["oci_core_instance.TestInstance"]
+#   vnic_id        = "${element(local.name[*].vnic_id,count.index)}"
+#   display_name   = "someDisplayName${count.index}"
+#   hostname_label = "somehostnamelabel${count.index}"
 
-  # provisioner "local-exec" {
-  #     command = "touch privateips/ifcfg-ens3:${count.index}\necho DEVICE='\"ens3:${count.index}\"' >> privateips/ifcfg-ens3:${count.index}\necho BOOTPROTO=static >> privateips/ifcfg-ens3:${count.index}\necho IPADDR=${self.ip_address} >> privateips/ifcfg-ens3:${count.index}\necho NETMASK=255.255.255.0 >> privateips/ifcfg-ens3:${count.index}\necho ONBOOT=yes >> privateips/ifcfg-ens3:${count.index}"  
-  # }
-}
+#   # provisioner "local-exec" {
+#   #     command = "touch privateips/ifcfg-ens3:${count.index}\necho DEVICE='\"ens3:${count.index}\"' >> privateips/ifcfg-ens3:${count.index}\necho BOOTPROTO=static >> privateips/ifcfg-ens3:${count.index}\necho IPADDR=${self.ip_address} >> privateips/ifcfg-ens3:${count.index}\necho NETMASK=255.255.255.0 >> privateips/ifcfg-ens3:${count.index}\necho ONBOOT=yes >> privateips/ifcfg-ens3:${count.index}"  
+#   # }
+# }
 
 
 
