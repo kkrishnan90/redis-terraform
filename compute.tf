031b7dfd12cf1345427bae57c972517daa67364e
kriskrku <kriskrku@KRISKRKU-IN.in.oracle.com>
2019-11-08 03:59:53 +0530

    added load balancer

--- compute.tf
+++ compute.tf
@@ -182,11 +182,11 @@ output "LB-1" {
 }
 
 output "LB-BackendSet-1" {
-  value = "${oci_load_balancer_backend_set.lb-http-backendset.*.ip_address}"
+  value = "${oci_load_balancer_backend_set.lb-http-backendset.backend.*.ip_address}"
 }
 
 output "LB-BackendSet-2" {
-  value = "${oci_load_balancer_backend_set.lb-ws-backendset.*.ip_address}"
+  value = "${oci_load_balancer_backend_set.lb-ws-backendset.backend.*.ip_address}"
 }
 
 
