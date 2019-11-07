0f2a84d6b5852f92abb2d3774866a59aa18953eb
kriskrku <kriskrku@KRISKRKU-IN.in.oracle.com>
2019-11-08 00:24:41 +0530

    final code for haproxy and redis backup

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,25 @@
 #! /bin/bash
 
+echo "Emptying contents of hosts.yml in ansible..."
 > ansible/hosts.yml
+sleep 1
+echo "Emptying contents of privateips generated templates..."
 rm -rf privateips/*
-app_servers_count=wc -l ansible/app-servers.conf | awk '{ print $1 }'
-$app_servers_count % 2 ? echo "Good to go ahead !" : echo "App servers cannot be odd in number"
+sleep 1
+echo "How many HAProxy do you want to provision ?"
+read hap_count
+echo "Checking app server count matches with HAProxy..."
+app_count=$(wc -l ansible/app-servers.conf | awk '{print $1}')
+sleep 1
+if (($hap_count < $app_count))
+then
+  echo "Risk forseen !!!!! Your HAProxy counts are less than app server counts. Modify /terraform/ansible/app-servers.conf file (Add more app servers) !"
+else
+  echo "Let's check once again before we proceed..."
+  if (($app_count % 2 == 0))
+  then
+    echo "Good to go ! Launch bash init.sh script to start provisioning infrastructure"
+  else
+    echo "Your app servers counts in odd number. Do you want to recheck ? If not, proceed launching bash init.sh script to provision infrastructure"
+  fi
+fi
