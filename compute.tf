ce909bd1bf6f8728ce1dd877df95a1775e3f8002
kriskrku <kriskrku@KRISKRKU-IN.in.oracle.com>
2019-11-07 12:48:17 +0530

    final

--- compute.tf
+++ compute.tf
@@ -53,13 +53,14 @@ resource "oci_core_private_ip" "private_ip" {
 }
 
 output "HAP-IPs" {
-  value = "${oci_core_instance.HAPInstance.*.private_ip[*]}"
+  value = "${oci_core_instance.HAPInstance.*.private_ip}"
 }
 
 
 resource "null_resource" "ansible_inventory" {
+  count = "${var.hap_instance_count}"
   provisioner "local-exec"{
-    command  = "bash create-host-ips.sh ${oci_core_instance.HAPInstance.*.private_ip[*]}"
+    command  = "bash create-host-ips.sh ${oci_core_instance.HAPInstance.*.private_ip[count.index]}"
     # echo  ${oci_core_instance.HAPInstance.*.private_ip} | cut -d \"|\" -f 2>> hosts.yml
   }
 }
