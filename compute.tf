79089797603e18b05639fceede9d05ab8d2e496b
kriskrku <kriskrku@KRISKRKU-IN.in.oracle.com>
2019-11-08 03:24:36 +0530

    added load balancer

--- compute.tf
+++ compute.tf
@@ -168,8 +168,12 @@ resource "oci_load_balancer_backend" "lb_backend2" {
   port             = "80"
 }
 
-output "LB-Backend1" {
+output "LB-BackendSet-1" {
   value = "${oci_load_balancer_backend.lb_backend1.*.name}"
+}
+
+output "LB-BackendSet-2" {
+  value = "${oci_load_balancer_backend.lb_backend2.*.name}"
 }
 
 
