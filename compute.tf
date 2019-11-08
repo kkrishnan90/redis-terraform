4cc2ef95f29ae189e39b48cc50b3fcbeebab62ef
kriskrku <kriskrku@KRISKRKU-IN.in.oracle.com>
2019-11-08 12:19:37 +0530

    added load balancer

--- compute.tf
+++ compute.tf
@@ -154,7 +154,7 @@ resource "oci_load_balancer_backend" "lb_backendhttp" {
   count = "${var.hap_instance_count * var.load_balancer_count}"
   #Required
   backendset_name  = "${oci_load_balancer_backend_set.lb-http-backendset.*.name[count.index % var.load_balancer_count]}"
-  ip_address       = "${lookup(element(oci_core_instance.HAPInstance, count.index),"private_ip")}"
+  ip_address       = "${lookup(element(oci_core_instance.HAPInstance, count.index % var.load_balancer_count),"private_ip")}"
   load_balancer_id = "${lookup(element(oci_load_balancer.lb1, count.index % var.load_balancer_count),"id")}"
   port             = "80"
 }
@@ -163,7 +163,7 @@ resource "oci_load_balancer_backend" "lb_backendws" {
   count = "${var.hap_instance_count * var.load_balancer_count}"
   #Required
   backendset_name  = "${oci_load_balancer_backend_set.lb-ws-backendset.*.name[count.index % var.load_balancer_count]}"
-  ip_address       = "${lookup(element(oci_core_instance.HAPInstance, count.index),"private_ip")}"
+  ip_address       = "${lookup(element(oci_core_instance.HAPInstance, count.index % var.load_balancer_count),"private_ip")}"
   load_balancer_id = "${lookup(element(oci_load_balancer.lb1, count.index % var.load_balancer_count),"id")}"
   port             = "80"
 }
