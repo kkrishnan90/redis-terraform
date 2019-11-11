629b0cf13c791920fd5e3daa6434f00d0e4ca5de
kriskrku <kriskrku@KRISKRKU-IN.in.oracle.com>
2019-11-11 21:44:48 +0530

    add bootscript ulimit data

--- bootscript.sh
+++ bootscript.sh
@@ -5,4 +5,3 @@ echo "root hard nofile 10000000" >> /etc/security/limits.conf
 echo "net.ipv4.ip_local_port_range = 1024 65535" >> /etc/sysctl.conf
 echo "fs.file-max = 10000000" >> /etc/sysctl.conf
 echo "fs.nr_open = 10000000" >> /etc/sysctl.conf
-reboot
