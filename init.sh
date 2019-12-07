1015648fc78733c25ce8ac394d99bc5dbaa96080
kriskrku <kriskrku@KRISKRKU-IN.in.oracle.com>
2019-12-07 18:07:24 +0530

    final working commit

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ terraform plan
 terraform output
 terraform apply
 sleep 5
-echo "Running playbook to configure HAProxy..."
+echo "Running playbook to copy deploy scripts to app servers..."
 bash run-appbook1.sh
 sleep 3
 echo "Running playbook to configure HAProxy..."
