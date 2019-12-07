502ad657c9abd4e2cc1f0cc153bebe855f9ca3bf
kriskrku <kriskrku@KRISKRKU-IN.in.oracle.com>
2019-12-07 22:46:07 +0530

    final working commit

--- cleanup.sh
+++ cleanup.sh
@@ -3,6 +3,7 @@
 echo "Emptying contents of hosts.yml and app-servers.conf in ansible..."
 > ansible/hosts.yml
 > ansible/app-servers.conf
+> ansible/app-hosts.yml
 sleep 1
 echo "Emptying contents of privateips generated templates..."
 rm -rf privateips/*
