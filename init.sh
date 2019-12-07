e4b2e20bc17ed8cfd9f3ffe64529f9eca3d99fda
kriskrku <kriskrku@KRISKRKU-IN.in.oracle.com>
2019-12-07 15:22:33 +0530

    final working commit

--- init.sh
+++ init.sh
@@ -7,5 +7,5 @@ sleep 5
 echo "Running playbook to configure HAProxy..."
 bash run-playbook1.sh
 sleep 3
-echo "Running cleanup script to cleanup directories..."
-bash cleanup.sh
+# echo "Running cleanup script to cleanup directories..."
+# bash cleanup.sh
