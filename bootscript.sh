f103dc2a3038a08b8cdc735674690fe55f34ca18
kriskrku <kriskrku@KRISKRKU-IN.in.oracle.com>
2019-12-07 22:27:23 +0530

    final working commit

--- bootscript.sh
+++ bootscript.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 sudo su
+echo "* soft nofile 10000000" >> /etc/security/limits.conf
+echo "* hard nofile 10000000" >> /etc/security/limits.conf
 echo "root soft nofile 10000000" >> /etc/security/limits.conf
 echo "root hard nofile 10000000" >> /etc/security/limits.conf
 echo "fs.file-max = 10000000" >> /etc/sysctl.conf
