19c2e0993762a64c6ab7af282f2be9525d250b92
Sherin Chandy <sherinchandy@gmail.com>
2020-01-16 13:18:55 +0000

    Update bootscript.sh

--- bootscript.sh
+++ bootscript.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
-sudo su
+sudo su -
 echo "* soft nofile 10000000" >> /etc/security/limits.conf
 echo "* hard nofile 10000000" >> /etc/security/limits.conf
 echo "root soft nofile 10000000" >> /etc/security/limits.conf
 echo "root hard nofile 10000000" >> /etc/security/limits.conf
 echo "fs.file-max = 10000000" >> /etc/sysctl.conf
 echo "fs.nr_open = 10000000" >> /etc/sysctl.conf
+sysctl -p
